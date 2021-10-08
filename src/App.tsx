fa2e6fae1e0ec73ca161f0df31f7e1cc4ff62364
AudreyFelicio <audreyanwar@gmail.com>
2021-10-09 00:26:39 +0800

    build(deps): Add essential packages, linting, and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React from "react";
+
+import logo from "./logo.svg";
+
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,6 +23,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
