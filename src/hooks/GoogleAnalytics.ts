3bba4806748e7806c4d253e20e4c7cb9da58037d
AudreyFelicio <audreyanwar@gmail.com>
2021-10-09 10:51:02 +0800

    chore: Address PR review

--- src/hooks/GoogleAnalytics.ts
+++ src/hooks/GoogleAnalytics.ts
@@ -4,5 +4,5 @@ import ReactGA from "react-ga";
 export const useTrackPage = (): void => {
   useEffect(() => {
     ReactGA.pageview(window.location.pathname + window.location.search);
-  });
+  }, []);
 };
