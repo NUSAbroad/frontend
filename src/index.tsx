020f1d285c73752c206030aeab3b6c835d720666
Chester How <chesterhow@gmail.com>
2021-10-11 11:46:18 +0800

    chore: set up redux persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import persistStore from "redux-persist/es/persistStore";
+import { PersistGate } from "redux-persist/integration/react";
 import { ThemeProvider } from "styled-components";
 
 import App from "./App";
+import { store } from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
 import GlobalStyle from "./styles/global-styles";
 import theme from "./styles/theme";
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
