8a3f23dbe52fab302ac3d010ae678b966f993282
AudreyFelicio <audreyanwar@gmail.com>
2021-10-11 14:05:31 +0800

    chore: Make navbar sticky and add drop shadow when scrolled

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import styled from "styled-components";
 
 import { Body1, Heading2, StyledLink } from "./Styles";
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ $boxShadow: boolean }>`
   position: sticky;
+  top: 0;
   width: 100%;
   display: flex;
   padding: 1rem;
+  box-shadow: ${(props) => (props.$boxShadow ? "0 4px 2px -2px gray" : "none")};
 `;
 
 const Logo = styled.div`
@@ -29,9 +31,25 @@ const Navbar: React.FC = () => {
   const currentPageLinkId = location.pathname.split("/")[1];
   const [underlinedTextId, setUnderlinedTextId] =
     useState<string>(currentPageLinkId);
+  const [boxShadow, setBoxShadow] = useState<boolean>(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    const scrollTop = document.documentElement.scrollTop;
+    if (scrollTop === 0) {
+      setBoxShadow(false);
+    } else {
+      setBoxShadow(true);
+    }
+  };
 
   return (
-    <Wrapper>
+    <Wrapper $boxShadow={boxShadow}>
       <Logo>
         <Heading2>
           <StyledLink to="/">NUSAbroad</StyledLink>
