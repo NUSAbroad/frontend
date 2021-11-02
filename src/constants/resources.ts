a4dfc6668a77b9cc26c14685549724961dff49d0
Chester How <chesterhow@gmail.com>
2021-11-03 06:17:16 +0800

    feat: add new resources and update footer (#95)

--- src/constants/resources.ts
+++ src/constants/resources.ts
@@ -7,26 +7,38 @@ export const resourcesContent = [
         label: "GRO Essential Guide PDF",
         url: "https://www.nus.edu.sg/gro/docs/default-source/pub/eginternational.pdf?sfvrsn=657cae7_4",
       },
+      {
+        label: "GRO SEP Student Reports",
+        url: "https://nusu.sharepoint.com/sites/ng/SEP%20Report/Forms/AllItems.aspx",
+      },
     ],
   },
   {
     header: "Faculty-Specific SEP Websites",
     links: [
       {
-        label: "School of Computing",
-        url: "https://www.comp.nus.edu.sg/programmes/ug/beyond/sep/",
+        label: "Business School",
+        url: "https://bba.nus.edu.sg/student-experience/exchange-programmes/about-us/",
       },
       {
         label: "Faculty of Arts and Social Sciences",
         url: "https://fass.nus.edu.sg/student-exchange-programme-sep/",
       },
       {
+        label: "Faculty of Engineering",
+        url: "https://www.eng.nus.edu.sg/undergraduate/degree-programmes/study-abroad/",
+      },
+      {
+        label: "Faculty of Law",
+        url: "https://law1.nus.edu.sg/student_matters/student_ex/outgoing_exchanges.html",
+      },
+      {
         label: "Faculty of Science",
         url: "https://www.science.nus.edu.sg/undergraduates/study-abroad-programmes/outgoing/student-exchange-programme-sep/",
       },
       {
-        label: "Business School",
-        url: "https://bba.nus.edu.sg/student-experience/exchange-programmes/about-us/",
+        label: "School of Computing",
+        url: "https://www.comp.nus.edu.sg/programmes/ug/beyond/sep/",
       },
       {
         label: "School of Design and Environment",
@@ -36,6 +48,15 @@ export const resourcesContent = [
   },
   {
     header: "Community Resources",
-    links: [{ label: "ExchangeBuddy", url: "https://www.exchangebuddy.com/" }],
+    links: [
+      {
+        label: "ExchangeBuddy",
+        url: "https://www.exchangebuddy.com/",
+      },
+      {
+        label: "SEPicker",
+        url: "https://sepicker.herokuapp.com/",
+      },
+    ],
   },
 ];
