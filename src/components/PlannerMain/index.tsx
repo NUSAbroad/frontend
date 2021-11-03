1d71a8c0856ceb3b10f43fa0289d4e53e4127b03
Chester How <chesterhow@gmail.com>
2021-11-03 16:35:47 +0800

    chore: improve empty state text

--- src/components/PlannerMain/index.tsx
+++ src/components/PlannerMain/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router";
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
 
 import { ReactComponent as UniversityIcon } from "../../assets/university.svg";
 import { View } from "../../constants/plannerViews";
@@ -8,7 +8,7 @@ import { useAppSelector } from "../../redux/hooks";
 import { getCurrView, getUnis } from "../../redux/plannerSlice";
 import MappingsView from "../MappingsView";
 import SideBySideView from "../SideBySideView";
-import { Body1, Button, Heading3 } from "../Styles";
+import { Body1, Button, Heading3, Subheading } from "../Styles";
 import ViewToggle from "./components/ViewToggle";
 
 const HeaderSection = styled.div`
@@ -28,21 +28,27 @@ const NoUniversityWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 0.8rem;
-  width: 100%;
-  margin: 10vh 0 5vh;
+  width: 450px;
+  max-width: 100%;
+  text-align: center;
+  margin: 10vh auto 5vh;
 
   @media (max-width: ${(props) => props.theme.breakPoints.lg}) {
-    margin: 10vh 0;
+    margin: 10vh auto;
   }
 `;
 
+const Text = styled.div`
+  margin: 1rem 0;
+`;
+
 const StyledUniversityIcon = styled(UniversityIcon)`
-  max-width: 50%;
+  max-width: 80%;
   height: auto;
 `;
 
 const PlannerMain: React.FC = function () {
+  const theme = useTheme();
   const currView = useAppSelector(getCurrView);
   const unis = useAppSelector(getUnis);
   const history = useHistory();
@@ -60,7 +66,18 @@ const PlannerMain: React.FC = function () {
       {unis.length === 0 && (
         <NoUniversityWrapper>
           <StyledUniversityIcon />
-          <Body1>You have yet to add any universities.</Body1>
+          <Text>
+            <Subheading $color={theme.colors.grey500}>
+              You&apos;ve yet to add any unis
+            </Subheading>
+            <Body1
+              $color={theme.colors.grey400}
+              style={{ marginTop: "0.4rem" }}
+            >
+              Add unis to your planner and you can compare them side-by-side or
+              customise mappings here
+            </Body1>
+          </Text>
           <Button onClick={onFindUniversitiesClickHandler}>
             Find Universities
           </Button>
